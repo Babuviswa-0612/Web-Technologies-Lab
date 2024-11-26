3f9f70bb0cc5679a569dbd20870dba0a668cfbfa
BABUVISWA B L <128886075+Babuviswa-0612@users.noreply.github.com>
2024-11-26 15:47:55 +0530

    Delete Ex_5 PhP & XML/database.sql

--- Ex_5 PhP & XML/database.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE photography;
-
-USE photography;
-
-CREATE TABLE favorite_cameras (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    camera_model VARCHAR(100) NOT NULL
-);
